219753cf9057e27fd1df02a1b69ee8ffcf437b49
rainmaker514 <tdoug016@fiu.edu>
2021-09-11 21:43:26 -0400

    reverting to 128f4ee0ac2cb25eb0e8db89d10e365d512bb80a

--- Nutrition-Tracker-Web/src/app/app.module.ts
+++ Nutrition-Tracker-Web/src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserService } from './service/user.service';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserListComponent,
-    UserFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
