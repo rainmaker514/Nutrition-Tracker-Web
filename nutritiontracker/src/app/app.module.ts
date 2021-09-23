753c656d3ed7bf25342bca04415a8173c109f7a2
rainmaker514 <tdoug016@fiu.edu>
2021-09-23 01:19:27 -0400

    added header and footer components

--- nutritiontracker/src/app/app.module.ts
+++ nutritiontracker/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserService } from './service/user.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserFormComponent
+    UserFormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
