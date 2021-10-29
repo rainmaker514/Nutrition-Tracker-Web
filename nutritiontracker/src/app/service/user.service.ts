3ea2750e0d473f4b7f9503181871bbcc4b819a4e
rainmaker514 <tdoug016@fiu.edu>
2021-10-29 19:11:29 -0400

    added addUsersToLocalCache method

--- nutritiontracker/src/app/service/user.service.ts
+++ nutritiontracker/src/app/service/user.service.ts
@@ -24,13 +24,15 @@ export class UserService {
     return this.http.put<User>(`${this.host}/users/update`, formData);
  }
 
-  resetPassword(email: string): Observable<any | HttpErrorResponse> {
+ resetPassword(email: string): Observable<any | HttpErrorResponse> {
    return this.http.get(`${this.host}/users/reset-password/${email}`);
-}
+ }
 
- deleteUser(id: number): Observable<void> {
+ deleteUser(id: number): Observable<void | HttpErrorResponse> {
     return this.http.delete<void>(`${this.host}/users/delete/${id}`);
  }
 
-
+ addUsersToLocalCache(users: User[]): void {
+   localStorage.setItem('users', JSON.stringify(users));
+}
 }
