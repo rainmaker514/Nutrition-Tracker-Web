0b1acda8b309968aa2d32703f620bf6066c4a0e6
rainmaker514 <tdoug016@fiu.edu>
2021-10-30 23:13:22 -0400

    added auth interceptor

---
+++ nutritiontracker/src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
+    if(httpRequest.url.includes(`${this.authenticationService.host}/user/login`)){
+      return httpHandler.handle(httpRequest);
+    }
+  }
+}
