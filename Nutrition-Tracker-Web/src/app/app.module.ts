f9adff952318971c4bbeefdc39efb1368b379547
rainmaker514 <tdoug016@fiu.edu>
2021-11-01 23:08:04 -0400

    added contact page

--- Nutrition-Tracker-Web/src/app/app.module.ts
+++ Nutrition-Tracker-Web/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './about/about/about.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
