7166fd320a87af90b1ac539e1958ee292d281842
rainmaker514 <tdoug016@fiu.edu>
2021-11-01 22:15:28 -0400

    added user class

--- Nutrition-Tracker-Web/src/app/guard/authentication.guard.ts
+++ Nutrition-Tracker-Web/src/app/guard/authentication.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { AuthenticationService } from '../service/authentication.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
