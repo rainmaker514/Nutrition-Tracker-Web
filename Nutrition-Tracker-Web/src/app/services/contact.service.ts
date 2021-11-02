e7a740607d263910039ae33a48deb641b3b6ebed
rainmaker514 <tdoug016@fiu.edu>
2021-11-01 22:49:58 -0400

    added contact service

--- Nutrition-Tracker-Web/src/app/services/contact.service.ts
+++ Nutrition-Tracker-Web/src/app/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contact } from '../models/contact';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  private host = environment.contactUrl;
+
+  constructor(private http: HttpClient) { }
+
+  //leave here for testing
+  //public findAll(): Observable<Contact[]> { return this.http.get<Contact[]>(`${this.contactUrl}/contact/all`); }
+
+  addContact(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(`${this.host}/contact/add`, contact);
+    }
 }
