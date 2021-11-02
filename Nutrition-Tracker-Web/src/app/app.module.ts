69ee4410f7bb602e8be9ef8ef9d402eea3f9fc79
rainmaker514 <tdoug016@fiu.edu>
2021-11-02 00:20:49 -0400

    set up routing and bulma

--- Nutrition-Tracker-Web/src/app/app.module.ts
+++ Nutrition-Tracker-Web/src/app/app.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { UserService } from './services/user.service';
+import { ContactService } from './services/contact.service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './pages/user/user.component';
 import { UsersComponent } from './pages/users/users.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthenticationService } from './services/authentication.service';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -27,9 +38,20 @@ import { UsersComponent } from './pages/users/users.component';
     UsersComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule,
+    CommonModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [UserService, ContactService, AuthenticationService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(){
+    library.add(faEnvelope, faLock);
+  }
+}
