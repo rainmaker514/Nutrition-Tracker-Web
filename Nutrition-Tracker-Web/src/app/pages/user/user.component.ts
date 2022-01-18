3917bc9e282f4d31099d5427d0f73f7e723ea973
rainmaker514 <tdoug016@fiu.edu>
2022-01-17 21:22:30 -0500

    added resetpassword http call

--- Nutrition-Tracker-Web/src/app/pages/user/user.component.ts
+++ Nutrition-Tracker-Web/src/app/pages/user/user.component.ts
@@ -1,11 +1,16 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faDumbbell, faEnvelope, faHourglass, faLock, faRuler, faUser, faWeight } from '@fortawesome/free-solid-svg-icons';
 import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 import { NotificationType } from 'src/app/enum/notification-type.enum';
+import { CustomHttpResponse } from 'src/app/models/custom-http-response';
 import { User } from 'src/app/models/user';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-pages-user',
@@ -14,7 +19,8 @@ import { NotificationService } from 'src/app/services/notification.service';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService, private router: Router, private notificationService: NotificationService) { }
+  constructor(private authenticationService: AuthenticationService, private router: Router, private notificationService: NotificationService, 
+    private userService: UserService) { }
 
   user: User;
   public heightDropDown: string[] = [];
@@ -26,6 +32,7 @@ export class UserComponent implements OnInit {
   faHourglass = faHourglass;
   faDumbbell = faDumbbell;
   faClipboard = faClipboard;
+  private subscriptions: Subscription[] = [];
 
   ngOnInit(): void {
     this.user = this.authenticationService.getUserFromLocalCache();
@@ -62,5 +69,18 @@ export class UserComponent implements OnInit {
         }
   }
 
-  
+  public onResetPassword(emailForm: NgForm): void{
+    const email = emailForm.value[''];
+    this.subscriptions.push(
+      this.userService.resetPassword(email).subscribe(
+        (response: CustomHttpResponse) => {
+          this.notificationService.notify(NotificationType.SUCCESS, response.message);
+        },
+        (error: HttpErrorResponse) => {
+          this.notificationService.notify(NotificationType.ERROR, error.error.message);
+        },
+        () => emailForm.reset()
+      )
+    );
+  }
 }
